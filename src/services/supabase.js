9a4cd8b28125a2b47a06236e068e5063654cfd54
chris-shirai <cks@vt.edu>
2024-12-18 22:00:31 -0500

    Temp commit to print key to console

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -6,7 +6,8 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://hbebmtiagssdrckajspr.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-
+console.log("Printing key");
+console.log(supabaseKey);
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
